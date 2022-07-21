0c8ff6168489c11af23fb21d8d9fd696fe3f476d
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-07-21 10:55:28 -0700

    commit tree ef588f904b5bad9b11aaeae497f2675498cd76f9

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string)
+}
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase()) 
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.indexOf(string) === 0 )
+}
